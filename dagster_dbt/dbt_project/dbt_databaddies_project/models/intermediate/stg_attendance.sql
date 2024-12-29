a87d5873623c8ad137098575016c8b3575f20167
Nancy Amandi <103530451+Nancy9ice@users.noreply.github.com>
2024-12-29 07:17:21 +0000

    added more dbt models

--- dagster_dbt/dbt_project/dbt_databaddies_project/models/intermediate/stg_attendance.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT
-    student_course_id,
-    total_minutes,
-    minutes_present,
-    created_at as attendance_created_at,
-    STRFTIME(created_at, '%Y%m%d') AS attendance_created_date_id,
-    updated_at as attendance_updated_at,
-    STRFTIME(updated_at, '%Y%m%d') AS attendance_updated_date_id
-FROM {{ source('dbt_databaddies_project', 'raw_attendance_day') }}
