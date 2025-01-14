5ed0f4db27954c32783e344bf10faaa9d5392951
Nancy Amandi <103530451+Nancy9ice@users.noreply.github.com>
2025-01-14 16:53:03 +0000

    edited dagster project

--- dagster_ml_dbt/setup.py
+++ dagster_ml_dbt/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dagster_ml_dbt",
-    packages=find_packages(include=["assets", "machine_learning", "dbt_databaddies_project"], exclude=["dagster_ml_dbt_tests"]),
+    packages=find_packages(include=["assets", "dbt_databaddies_project"], exclude=["dagster_ml_dbt_tests"]),
     install_requires=[
         "dagster",
         "dagster-cloud",
