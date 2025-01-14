a6b4d54e7a97a041a4e9f8ae5a6bd47bae3194db
Nancy Amandi <103530451+Nancy9ice@users.noreply.github.com>
2025-01-14 21:41:22 +0000

    edited dagster project

--- dagster_ml_dbt/jobs.py
+++ dagster_ml_dbt/jobs.py
@@ -3,12 +3,6 @@ from dagster import (
     AssetSelection,
 )
 from dagster_ml_dbt.assets.dbt_assets import my_dbt_assets
-from dagster_ml_dbt.assets.ml_assets import (fetch_sat_data, 
-                            preprocess_sat_data, 
-                            split_train_test, 
-                            train_rf_model_asset, 
-                            predict_and_generate_recommendations, 
-                            write_recommendations_to_motherduck)
 
 # Define job to run Airbyte sync and dbt build
 airbyte_dbt_sync_job = define_asset_job(
@@ -16,17 +10,4 @@ airbyte_dbt_sync_job = define_asset_job(
     AssetSelection.assets(my_dbt_assets)
     .upstream()
     .required_multi_asset_neighbors(),  # all Airbyte assets linked to the same connection
-    )
-
-
-# Define the job for SAT data assets
-sat_ml_pipeline = define_asset_job(
-    "sat_ml_pipeline_job",
-    AssetSelection.assets(fetch_sat_data, 
-                        preprocess_sat_data, 
-                        split_train_test,
-                        train_rf_model_asset,
-                        predict_and_generate_recommendations,
-                        write_recommendations_to_motherduck)
-    .upstream()
     )
